33ce8e3e76ce1b92ef0d8566daf186750959130f
mcmonkey <agoodwin@freneticllc.com>
2019-03-07 14:03:57 -0800

    compile function command

--- FreneticScript/CommandSystem/QueueCmds/FunctionCommand.cs
+++ FreneticScript/CommandSystem/QueueCmds/FunctionCommand.cs
@@ -6,6 +6,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using FreneticScript.TagHandlers;
 using FreneticScript.TagHandlers.Objects;
 using FreneticUtilities.FreneticExtensions;
@@ -79,17 +82,11 @@ namespace FreneticScript.CommandSystem.QueueCmds
         public FunctionCommand()
         {
             Name = "function";
-            Arguments = "'stop'/'define'/'undefine' [name of function] ['quiet_fail']";
+            Arguments = "'stop'/'define'/'undefine' [name of function]";
             Description = "Creates a new function of the following command block, and adds it to the script cache.";
             Asyncable = true;
             MinimumArguments = 1;
             MaximumArguments = 3;
-            ObjectTypes = new List<Func<TemplateObject, TemplateObject>>()
-            {
-                Verify1,
-                TextTag.For,
-                Verify2
-            };
         }
         
         /// <summary>
@@ -100,25 +97,68 @@ namespace FreneticScript.CommandSystem.QueueCmds
         public override void PreAdaptToCIL(CILAdaptationValues values, int entry)
         {
             CommandEntry cent = values.Entry.Entries[entry];
-            string arg0 = cent.Arguments[0].ToString();
-            // TODO: Compile this fully!
-            if (arg0 == "\0CALLBACK")
+            if (cent.IsCallback)
             {
                 values.PopVarSet();
-                // ...
+                return;
             }
-            else if (arg0 == "stop")
-            {
-                // ...
-            }
-            else if (arg0 == "undefine")
-            {
-                // ...
-            }
-            else // "define"
+            string arg0 = cent.Arguments[0].ToString();
+            if (arg0 == "define")
             {
                 values.PushVarSet();
-                // ...
+                // TODO: Forcibly compress the block to an argument, instead of compiling the sub-block
+            }
+            else if (arg0 != "undefine" && arg0 != "stop")
+            {
+                throw new ErrorInducedException("First argument must be 'define', 'undefine', or 'stop'.");
+            }
+        }
+        
+        /// <summary>
+        /// Represents the <see cref="DebugStop(CommandQueue, CommandEntry)"/> method.
+        /// </summary>
+        public static MethodInfo DebugStopMethod = typeof(FunctionCommand).GetMethod(nameof(DebugStop));
+
+        /// <summary>
+        /// Represents the <see cref="DebugCallback(CommandQueue, CommandEntry)"/> method.
+        /// </summary>
+        public static MethodInfo DebugCallbackMethod = typeof(FunctionCommand).GetMethod(nameof(DebugCallback));
+
+        /// <summary>
+        /// Represents the <see cref="Undefine(CommandEntry, CommandQueue)"/> method.
+        /// </summary>
+        public static MethodInfo UndefineMethod = typeof(FunctionCommand).GetMethod(nameof(Undefine));
+
+        /// <summary>
+        /// Represents the <see cref="Define(CommandEntry, CommandQueue)"/> method.
+        /// </summary>
+        public static MethodInfo DefineMethod = typeof(FunctionCommand).GetMethod(nameof(Define));
+
+        /// <summary>
+        /// Shows debug for a function 'stop' command.
+        /// </summary>
+        /// <param name="queue">The command queue.</param>
+        /// <param name="entry">The command entry.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DebugStop(CommandQueue queue, CommandEntry entry)
+        {
+            if (entry.ShouldShowGood(queue))
+            {
+                entry.GoodOutput(queue, "Stopping a function call.");
+            }
+        }
+
+        /// <summary>
+        /// Shows debug for a function callback.
+        /// </summary>
+        /// <param name="queue">The command queue.</param>
+        /// <param name="entry">The command entry.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DebugCallback(CommandQueue queue, CommandEntry entry)
+        {
+            if (entry.ShouldShowGood(queue))
+            {
+                entry.GoodOutput(queue, "Completed function call.");
             }
         }
 
@@ -130,39 +170,129 @@ namespace FreneticScript.CommandSystem.QueueCmds
         public override void AdaptToCIL(CILAdaptationValues values, int entry)
         {
             CommandEntry cent = values.Entry.Entries[entry];
-            if (cent.Arguments.Count == 1 && cent.Arguments[0].ToString() == "\0CALLBACK" && cent.BlockStart > 0)
+            bool db = cent.DBMode <= DebugMode.FULL;
+            if (cent.IsCallback)
             {
-                CommandEntry start = values.Entry.Entries[cent.BlockStart - 1];
-                if (start.Command is FunctionCommand && start.Arguments.Count > 0 && start.Arguments[0].ToString() == "define")
+                if (db)
                 {
-                    return;
+                    values.LoadQueue();
+                    values.LoadEntry(entry);
+                    values.ILGen.Emit(OpCodes.Call, DebugCallbackMethod);
                 }
+                return;
             }
-            base.AdaptToCIL(values, entry);
+            string arg0 = cent.Arguments[0].ToString();
+            if (arg0 == "stop")
+            {
+                for (int i = entry - 1; i >= 0; i--)
+                {
+                    CommandEntry nextEntry = values.Entry.Entries[i];
+                    if (!(nextEntry.Command is FunctionCommand))
+                    {
+                        continue;
+                    }
+                    if (nextEntry.IsCallback)
+                    {
+                        if (db)
+                        {
+                            values.LoadQueue();
+                            values.LoadEntry(entry);
+                            values.ILGen.Emit(OpCodes.Call, DebugStopMethod);
+                        }
+                        values.ILGen.Emit(OpCodes.Br, values.Entry.AdaptedILPoints[i + 2]);
+                        return;
+                    }
+                }
+                throw new ErrorInducedException("Invalid 'function stop' command: not inside a function block!");
+            }
+            else if (arg0 == "undefine")
+            {
+                if (cent.Arguments.Count < 2)
+                {
+                    throw new ErrorInducedException("Invalid 'function undefine' command: must label what function to undefine (as an additional argument)!");
+                }
+                values.LoadEntry(entry);
+                values.LoadQueue();
+                values.ILGen.Emit(OpCodes.Call, UndefineMethod);
+            }
+            else // "define"
+            {
+                if (cent.Arguments.Count < 2)
+                {
+                    throw new ErrorInducedException("Invalid 'function define' command: must name the function to define (as an additional argument)!");
+                }
+                if (cent.InnerCommandBlock == null)
+                {
+                    throw new ErrorInducedException("Invalid 'function define' command: must be followed by a block-function to define!");
+                }
+                values.LoadEntry(entry);
+                values.LoadQueue();
+                values.ILGen.Emit(OpCodes.Call, DefineMethod);
+                values.ILGen.Emit(OpCodes.Br, values.Entry.AdaptedILPoints[cent.BlockEnd + 2]);
+            }
         }
 
-        TemplateObject Verify1(TemplateObject input)
+        /// <summary>
+        /// Executes an undefine.
+        /// </summary>
+        /// <param name="queue">The command queue involved.</param>
+        /// <param name="entry">Entry to be executed.</param>
+        public static void Undefine(CommandEntry entry, CommandQueue queue)
         {
-            if (input.ToString() == "\0CALLBACK")
+            string name = entry.GetArgument(queue, 1).ToLowerFast();
+            if (!queue.Engine.Functions.Remove(name))
             {
-                return input;
+                if (BooleanTag.TryFor(entry.GetNamedArgumentObject(queue, "quiet_fail"))?.Internal ?? false)
+                {
+                    if (entry.ShouldShowGood(queue))
+                    {
+                        entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' doesn't exist!");
+                    }
+                }
+                else
+                {
+                    queue.HandleError(entry, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' doesn't exist!");
+                }
             }
-            string inp = input.ToString().ToLowerFast();
-            if (inp == "stop" || inp == "define" || inp == "undefine")
+            else
             {
-                return new TextTag(inp);
+                if (entry.ShouldShowGood(queue))
+                {
+                    entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' undefined.");
+                }
             }
-            return null;
         }
-        
-        TemplateObject Verify2(TemplateObject input)
+
+        /// <summary>
+        /// Executes a define.
+        /// </summary>
+        /// <param name="queue">The command queue involved.</param>
+        /// <param name="entry">Entry to be executed.</param>
+        public static void Define(CommandEntry entry, CommandQueue queue)
         {
-            string inp = input.ToString().ToLowerFast();
-            if (inp == "quiet_fail")
+            string name = entry.GetArgument(queue, 1).ToLowerFast();
+            if (queue.Engine.Functions.ContainsKey(name))
             {
-                return new TextTag(inp);
+                if (BooleanTag.TryFor(entry.GetNamedArgumentObject(queue, "quiet_fail"))?.Internal ?? false)
+                {
+                    if (entry.ShouldShowGood(queue))
+                    {
+                        entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' already exists!");
+                    }
+                }
+                else
+                {
+                    queue.HandleError(entry, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' already exists!");
+                }
             }
-            return null;
+            else
+            {
+                queue.Engine.Functions.Add(name, new CommandScript(name, CommandScript.TYPE_NAME_FUNCTION, entry.InnerCommandBlock, entry.BlockStart, entry.DBMode));
+                if (entry.ShouldShowGood(queue))
+                {
+                    entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' defined.");
+                }
+            }
         }
 
         /// <summary>
@@ -172,105 +302,7 @@ namespace FreneticScript.CommandSystem.QueueCmds
         /// <param name="entry">Entry to be executed.</param>
         public static void Execute(CommandQueue queue, CommandEntry entry)
         {
-            string type = entry.GetArgument(queue, 0);
-            if (type == "\0CALLBACK")
-            {
-                if (entry.ShouldShowGood(queue))
-                {
-                    entry.GoodOutput(queue, "Completed function call.");
-                }
-                return;
-            }
-            type = type.ToLowerFast();
-            if (type == "stop")
-            {
-                CommandStackEntry cse = queue.CurrentStackEntry;
-                for (int i = 0; i < cse.Entries.Length; i++)
-                {
-                    if (queue.GetCommand(i).Command is FunctionCommand && queue.GetCommand(i).Arguments[0].ToString() == "\0CALLBACK")
-                    {
-                        if (entry.ShouldShowGood(queue))
-                        {
-                            entry.GoodOutput(queue, "Stopping a function call.");
-                        }
-                        cse.Index = i + 2;
-                        return;
-                    }
-                }
-                queue.HandleError(entry, "Cannot stop function: not in one!");
-            }
-            else if (type == "undefine")
-            {
-                if (entry.Arguments.Count < 2)
-                {
-                    ShowUsage(queue, entry);
-                    return;
-                }
-                string name = entry.GetArgument(queue, 1).ToLowerFast();
-                if (!queue.Engine.Functions.ContainsKey(name))
-                {
-                    if (entry.Arguments.Count > 2 && entry.GetArgument(queue, 2).ToLowerFast() == "quiet_fail")
-                    {
-                        if (entry.ShouldShowGood(queue))
-                        {
-                            entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' doesn't exist!");
-                        }
-                    }
-                    else
-                    {
-                        queue.HandleError(entry, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' doesn't exist!");
-                    }
-                }
-                else
-                {
-                    queue.Engine.Functions.Remove(name);
-                    if (entry.ShouldShowGood(queue))
-                    {
-                        entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' undefined.");
-                    }
-                }
-            }
-            else if (type == "define")
-            {
-                if (entry.Arguments.Count < 2)
-                {
-                    ShowUsage(queue, entry);
-                    return;
-                }
-                string name = entry.GetArgument(queue, 1).ToLowerFast();
-                if (entry.InnerCommandBlock == null)
-                {
-                    queue.HandleError(entry, "Function invalid: No block follows!");
-                    return;
-                }
-                if (queue.Engine.Functions.ContainsKey(name))
-                {
-                    if (entry.Arguments.Count > 2 && entry.GetArgument(queue, 2).ToLowerFast() == "quiet_fail")
-                    {
-                        if (entry.ShouldShowGood(queue))
-                        {
-                            entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' already exists!");
-                        }
-                    }
-                    else
-                    {
-                        queue.HandleError(entry, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' already exists!");
-                    }
-                }
-                else
-                {
-                    queue.Engine.Functions.Add(name, new CommandScript(name, CommandScript.TYPE_NAME_FUNCTION, entry.InnerCommandBlock, entry.BlockStart, entry.DBMode));
-                    if (entry.ShouldShowGood(queue))
-                    {
-                        entry.GoodOutput(queue, "Function '" + TextStyle.Separate + name + TextStyle.Base + "' defined.");
-                    }
-                }
-                queue.CurrentStackEntry.Index = entry.BlockEnd + 2;
-            }
-            else
-            {
-                ShowUsage(queue, entry);
-            }
+            queue.HandleError(entry, "Cannot Execute() a function command, must compile!");
         }
     }
 }
