110abf4fd17f327e14842c2be7a71e2df00ccc00
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-05-07 01:47:25 -0700

    improve CleanMem command

--- FreneticScript/CommandSystem/CommonCmds/CleanmemCommand.cs
+++ FreneticScript/CommandSystem/CommonCmds/CleanmemCommand.cs
@@ -8,21 +8,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using FreneticScript.TagHandlers;
+using FreneticScript.TagHandlers.Objects;
+using FreneticUtilities.FreneticExtensions;
 
 namespace FreneticScript.CommandSystem.CommonCmds;
 
 // TODO: Meta!
 /// <summary>A command to interact with the system garbage collector.</summary>
+[CommandMeta(
+    Name = "cleanmem",
+    Arguments = "[mode]",
+    Short = "Forces the system Garbage Collector to run, invoking CPU usage to lower RAM usage.",
+    Updated = "2025/05/07",
+    Group = "Common",
+    Asyncable = true,
+    MinimumArgs = 0, MaximumArgs = 1,
+    Description = "The optional argument can be 'full' for aggressive full clean, or 'simple' (the default) for less aggressive clean, or 'gentle' for very minimal.",
+    Examples = [ "// Does an aggressive full clean immediately. May lag.\n"
+                    + "cleanmum full;"]
+    )]
 public class CleanmemCommand: AbstractCommand
 {
+    /// <summary>Set of strings allowed as the first arg.</summary>
+    public static HashSet<string> Modes = ["full", "simple", "gentle"];
+
     /// <summary>Constructs the command.</summary>
     public CleanmemCommand()
     {
-        Name = "cleanmem";
-        Arguments = "";
-        Description = "Forces the system Garbage Collector to run, invoking CPU usage to lower RAM usage.";
-        Asyncable = true;
+        static void validator(ArgumentValidation validator)
+        {
+            if (!Modes.Contains(validator.ObjectValue.ToString()))
+            {
+                validator.ErrorAction($"Invalid memory cleaning mode '{validator.ObjectValue}': must be one of [{Modes.JoinString(", ")}].");
+                return;
+            }
+            validator.ObjectValue = TextTag.For(validator.ObjectValue);
+        }
+        ObjectTypes =
+        [
+            validator
+        ];
     }
 
     /// <summary>Executes the command.</summary>
@@ -30,7 +55,24 @@ public class CleanmemCommand: AbstractCommand
     /// <param name="entry">Entry to be executed.</param>
     public static void Execute(CommandQueue queue, CommandEntry entry)
     {
-        GC.Collect();
+        string mode = entry.Arguments.Length == 0 ? "simple" : entry.GetArgument(queue, 0).ToLowerFast();
+        if (mode == "full")
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive, true, true);
+        }
+        else if (mode == "simple")
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, false, true);
+        }
+        else if (mode == "gentle")
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Optimized, false, false);
+        }
+        else
+        {
+            queue.HandleError(entry, $"Invalid memory cleaning mode '{mode}': must be one of [{Modes.JoinString(", ")}].");
+            return;
+        }
         if (entry.ShouldShowGood(queue))
         {
             entry.GoodOutput(queue, "Memory cleaned.");
